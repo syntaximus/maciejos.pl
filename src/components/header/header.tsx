3c76872ccfa8239939a762ad9c1d29d5be45c7ed
Maciej Jakubiak <maciej626@gmail.com>
2023-05-14 23:45:59 +0200

    sekcja z mediami społecznościowymi

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,22 @@
-import { Row, Col, Container } from 'react-bootstrap';
 import './header.css';
 import Hero from './hero';
 import Navigation from './navigation';
 import Socials from './socials';
 
-const Header = () => {
-  var elem = 'Header';
-  return (
-    <header className='sticky-top'>
-      <div className='header d-flex flex-column justify-content-sm-between'>
-        <div>
-          <Hero />
-          <div className='d-none d-lg-block'>
-            <Navigation />
-          </div>
-        </div>
-        <div>
-          <Socials />
+const Header = () => (
+  <header className='sticky-top'>
+    <div className='header d-flex flex-column justify-content-sm-between'>
+      <div>
+        <Hero />
+        <div className='d-none d-lg-block'>
+          <Navigation />
         </div>
       </div>
-    </header>
-  );
-};
+      <div>
+        <Socials />
+      </div>
+    </div>
+  </header>
+);
 
 export default Header;
--- src/components/header/socials.tsx
+++ src/components/header/socials.tsx
@@ -1,6 +1,26 @@
-const Socials = () => {
-  var elem = 'Socials section';
-  return <div className='socials'>{elem}</div>;
-};
+import { Col, Container, Row } from 'react-bootstrap';
+import { Github, Linkedin, Facebook } from '../../assets/icons';
+
+const Socials = () => (
+  <Container className='socials'>
+    <Row>
+      <Col xs='auto' className='px-2'>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/syntaximus/'>
+          <Linkedin style={{ width: 30, height: 30, color: 'var(--app-text-main-color)' }} />
+        </a>
+      </Col>
+      <Col xs='auto' className='px-2'>
+        <a target='_blank' rel='noopener noreferrer' href='https://github.com/syntaximus'>
+          <Github style={{ width: 30, height: 30, color: 'var(--app-text-main-color)' }} />
+        </a>
+      </Col>
+      <Col xs='auto' className='px-2'>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/maciejjos'>
+          <Facebook style={{ width: 30, height: 30, color: 'var(--app-text-main-color)' }} />
+        </a>
+      </Col>
+    </Row>
+  </Container>
+);
 
 export default Socials;
