5de428306909e63b95073dd8850496bad1dd9662
Maciej Jakubiak <maciej626@gmail.com>
2023-05-22 18:57:04 +0200

    przeniesienie przycisku perspektywistycznego na dół ekranu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -20,24 +20,26 @@ const Header = (props: IHeaderProps) => (
             <div className='d-none d-lg-block'>
               <Navigation />
             </div>
-            <Form.Check
-              className='pb-3'
-              type='switch'
-              id='perspective-switch'
-              label='Widok perspektywistyczny'
-              checked={props.cinematicMode}
-              onChange={() => {
-                document.body.style.overflow = props.cinematicMode ? '' : 'hidden';
-                document.body.style.height = props.cinematicMode ? '' : '100%';
-                document.body.style.width = props.cinematicMode ? '' : '100%';
-                props.setCinematicMode((prev) => !prev);
-              }}
-            />
           </>
         )}
       </div>
       <div>
         <Socials />
+        {!props.cinematicMode && (
+          <Form.Check
+            className='pb-2 perspective-switch'
+            type='switch'
+            id='perspective-switch'
+            label='Widok perspektywistyczny'
+            checked={props.cinematicMode}
+            onChange={() => {
+              document.body.style.overflow = props.cinematicMode ? '' : 'hidden';
+              document.body.style.height = props.cinematicMode ? '' : '100%';
+              document.body.style.width = props.cinematicMode ? '' : '100%';
+              props.setCinematicMode((prev) => !prev);
+            }}
+          />
+        )}
       </div>
     </div>
   </header>
