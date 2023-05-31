61eced179648cc166ed075bfbeb4b30fc8994d73
Maciej Jakubiak <maciej626@gmail.com>
2023-05-31 03:51:08 +0200

    tłumaczenie na angielski sekcji o mnie

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,47 +1,74 @@
+import { Trans, useTranslation } from 'react-i18next';
 import './about.css';
 
 export interface IAboutProps {
   cinematicMode: boolean;
 }
 
-const About = (props: IAboutProps) => (
-  <div id='about' className='about'>
-    <h4 className={'text-highlight-color' + (!props.cinematicMode ? ' section-header' : '')}>O mnie</h4>
-    <p className='text-justify'>
-      Od najmłodszych lat interesuję się technologiami. Dzięki zacięciu matematycznemu{' '}
-      <span className='text-nowrap'>i informatycznemu</span>, ukończyłem informatykę{' '}
-      <span className='text-nowrap'>na wydziale</span>{' '}
-      <a target='_blank' rel='noopener noreferrer' href='https://ww2.mini.pw.edu.pl/'>
-        Matematyki <span className='text-nowrap'>i Nauk</span> Informacyjnych Politechniki Warszawskiej
-      </a>
-      . Programowanie to moja pasja <span className='text-nowrap'>i implementacja</span> nowych, ciekawych rozwiązań
-      daje mi dużo satysfakcji.
-    </p>
-    <p className='text-justify'>
-      Zawodowo zajmuję się tworzeniem aplikacji webowych <span className='text-nowrap'>w środowisku</span> .NET zarówno{' '}
-      <span className='text-nowrap'>od strony</span> backendowej,{' '}
-      <span className='text-nowrap'>jak i frontendowej</span>. Zrealizowałem wiele projektów,{' '}
-      <span className='text-nowrap'>z których</span> korzystają setki tysięcy ludzi. Skupiam się{' '}
-      <span className='text-nowrap'>na optymalizacji</span> procesów <span className='text-nowrap'>i tworzeniu</span>{' '}
-      wydajnego kodu. Cyberbezpieczeństwo to mój konik <span className='text-nowrap'>i dbam</span> o zachowanie wysokich
-      standardów <span className='text-nowrap'>w tej</span> dziedzinie w swoich rozwiązaniach.
-    </p>
-    <p className='text-justify'>
-      W swoich projektach stawiam na konteneryzację i administruję własnym klastrem{' '}
-      <a target='_blank' rel='noopener noreferrer' href='https://kubernetes.io/'>
-        Kubernetes
-      </a>
-      . Obecnie zajmuję się tworzeniem serwisu webowego, umożliwiającego kontrolowanie wydatków - aplikację{' '}
-      <a className='text-nowrap' href='/#projects'>
-        Pan Rachunek
-      </a>
-      .
-    </p>
-    <p className='text-justify'>
-      Gdy nie ma mnie przy komputerze najczęściej zajmuję się swoim hobby jakim jest triathlon,{' '}
-      <span className='text-nowrap'>a dokładniej</span> trening na pełen dystans Ironmana.
-    </p>
-  </div>
-);
+const About = (props: IAboutProps) => {
+  useTranslation();
+  return (
+    <div id='about' className='about'>
+      <h4 className={'text-highlight-color' + (!props.cinematicMode ? ' section-header' : '')}>
+        <Trans i18nKey='about.section'>O mnie</Trans>
+      </h4>
+      <p className='text-justify'>
+        <Trans
+          i18nKey='about.first-paragraph'
+          components={[
+            <span className='text-nowrap' />,
+            <a target='_blank' rel='noopener noreferrer' href='https://ww2.mini.pw.edu.pl/' />,
+          ]}
+        >
+          Od najmłodszych lat interesuję się technologiami. Dzięki zacięciu matematycznemu{' '}
+          <span className='text-nowrap'>i informatycznemu</span>, ukończyłem informatykę{' '}
+          <span className='text-nowrap'>na wydziale</span>{' '}
+          <a target='_blank' rel='noopener noreferrer' href='https://ww2.mini.pw.edu.pl/'>
+            Matematyki <span className='text-nowrap'>i Nauk</span> Informacyjnych Politechniki Warszawskiej
+          </a>
+          . Programowanie to moja pasja <span className='text-nowrap'>i implementacja</span> nowych, ciekawych rozwiązań
+          daje mi dużo satysfakcji.
+        </Trans>
+      </p>
+      <p className='text-justify'>
+        <Trans i18nKey='about.second-paragraph' components={[<span className='text-nowrap' />]}>
+          Zawodowo zajmuję się tworzeniem aplikacji webowych <span className='text-nowrap'>w środowisku</span> .NET
+          zarówno <span className='text-nowrap'>od strony</span> backendowej,{' '}
+          <span className='text-nowrap'>jak i frontendowej</span>. Zrealizowałem wiele projektów,{' '}
+          <span className='text-nowrap'>z których</span> korzystają setki tysięcy ludzi. Skupiam się{' '}
+          <span className='text-nowrap'>na optymalizacji</span> procesów{' '}
+          <span className='text-nowrap'>i tworzeniu</span> wydajnego kodu. Cyberbezpieczeństwo to mój konik{' '}
+          <span className='text-nowrap'>i dbam</span> o zachowanie wysokich standardów{' '}
+          <span className='text-nowrap'>w tej</span> dziedzinie w swoich rozwiązaniach.
+        </Trans>
+      </p>
+      <p className='text-justify'>
+        <Trans
+          i18nKey='about.third-paragraph'
+          components={[
+            <a target='_blank' rel='noopener noreferrer' href='https://kubernetes.io/' />,
+            <a target='_blank' rel='noopener noreferrer' href='/#projects' />,
+          ]}
+        >
+          W swoich projektach stawiam na konteneryzację i administruję własnym klastrem{' '}
+          <a target='_blank' rel='noopener noreferrer' href='https://kubernetes.io/'>
+            Kubernetes
+          </a>
+          . Obecnie zajmuję się tworzeniem serwisu webowego, umożliwiającego kontrolowanie wydatków - aplikację{' '}
+          <a className='text-nowrap' href='/#projects'>
+            Pan Rachunek
+          </a>
+          .
+        </Trans>
+      </p>
+      <p className='text-justify'>
+        <Trans i18nKey='about.fourth-paragraph' components={[<span className='text-nowrap' />]}>
+          Gdy nie ma mnie przy komputerze najczęściej zajmuję się swoim hobby jakim jest triathlon,{' '}
+          <span className='text-nowrap'>a dokładniej</span> trening na pełen dystans Ironmana.
+        </Trans>
+      </p>
+    </div>
+  );
+};
 
 export default About;
